2010dc27ab250236ea1ede79b02bd6d77c455eb8
Angel <angelbedo104@gmail.com>
2023-12-08 16:16:38 -0500

    Avanzando en guardar de productos

---
+++ src/main/java/com/curso/eccomerce/demo/repository/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.curso.eccomerce.demo.repository;
+
+import com.curso.eccomerce.demo.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+}
---
+++ src/main/java/com/curso/eccomerce/demo/service/ProductoService.java
@@ -0,0 +1,12 @@
+package com.curso.eccomerce.demo.service;
+
+import com.curso.eccomerce.demo.model.Producto;
+
+import java.util.Optional;
+
+public interface ProductoService {
+    public Producto save (Producto producto);
+    public Optional<Producto> get(Integer id);
+    public void update(Producto producto);
+    public void delete(Integer id);
+}
